68a26586089651aa206abd703f5e5eb5ca9b4413
GabiiCata <gabiicata94@gmail.com>
2025-02-12 19:18:42 -0300

    first commit

---
+++ src/test/java/com/w2m/naves/NavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.naves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
