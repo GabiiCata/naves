eb08487a6664285ae5d9e66aa3ca6d942285a6c4
Gabriel Espina <59236737+GabiiCata@users.noreply.github.com>
2025-02-26 15:16:18 -0300

    Feat/pending features (#4)
    * feat: add rabbitmq configuration (#2)
    
    * Feat/enable cache usage (#3)
    
    * feat: add spring cache simple
    
    * doc: update readme

--- src/main/java/com/w2m/naves/configuration/SecurityConfig.java
+++ src/main/java/com/w2m/naves/configuration/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
--- src/main/java/com/w2m/naves/spaceship/application/SpaceshipService.java
+++ src/main/java/com/w2m/naves/spaceship/application/SpaceshipService.java
@@ -1,10 +1,15 @@
 package com.w2m.naves.spaceship.application;
 
+import com.w2m.naves.mq.SpaceShipProducer;
 import com.w2m.naves.spaceship.domain.EOrigin;
 import com.w2m.naves.spaceship.domain.Spaceship;
+import com.w2m.naves.spaceship.infrastructure.exception.SpaceshipNotFoundException;
 import com.w2m.naves.spaceship.infrastructure.repository.SpaceshipRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,16 +22,21 @@ import java.util.Optional;
 public class SpaceshipService {
 
     private final SpaceshipRepository spaceshipRepository;
+    private final SpaceShipProducer spaceShipProducer;
+
     private final ModelMapper modelMapper;
 
+    @Cacheable(value = "spaceships")
     public Page<SpaceshipDTO> getAllSpaceships(Pageable pageable) {
         return spaceshipRepository.findAll(pageable)
                 .map(spaceship -> modelMapper.map(spaceship, SpaceshipDTO.class));
     }
 
-    public Optional<SpaceshipDTO> getSpaceshipById(Long id) {
+    @Cacheable(value = "spaceships", key = "#id")
+    public SpaceshipDTO getSpaceshipById(Long id) {
         return spaceshipRepository.findById(id)
-                .map(spaceship -> modelMapper.map(spaceship, SpaceshipDTO.class));
+                .map(spaceship -> modelMapper.map(spaceship, SpaceshipDTO.class))
+                .orElseThrow(() -> new SpaceshipNotFoundException(id));
     }
 
 
@@ -44,25 +54,40 @@ public class SpaceshipService {
 
     public SpaceshipDTO createSpaceship(SpaceshipDTO spaceship) {
         Spaceship spaceshipDb =  spaceshipRepository.save(modelMapper.map(spaceship,Spaceship.class));
+        spaceShipProducer.sendSpaceShipMessage( "nave "+spaceshipDb.getSpaceshipId()+" creada" );
         return modelMapper.map(spaceshipDb,SpaceshipDTO.class);
     }
 
+    @CachePut(value = "spaceships", key = "#id")
+    @CacheEvict(value = "spaceships", allEntries = true)
     public SpaceshipDTO updateSpaceship(Long id, SpaceshipDTO spaceshipDetails) {
-        return spaceshipRepository.findById(id).map(spaceship -> {
-            if ( spaceshipDetails.getName()!= null)
-                spaceship.setName(spaceshipDetails.getName());
-            if ( spaceshipDetails.getDescription() !=  null )
-                spaceship.setDescription(spaceshipDetails.getDescription());
-            if (spaceshipDetails.getOrigin() != null)
-                spaceship.setOrigin(spaceshipDetails.getOrigin());
-            Spaceship updatedSpaceship = spaceshipRepository.saveAndFlush(spaceship);
-            return modelMapper.map(updatedSpaceship, SpaceshipDTO.class);
-        }).orElseThrow(() -> new RuntimeException("Nave no encontrada con ID: " + id));
+        Optional<Spaceship> spaceshipOptional = spaceshipRepository.findById(id);
+        if (spaceshipOptional.isEmpty())
+            throw new SpaceshipNotFoundException(id);
+
+        Spaceship spaceship = spaceshipOptional.get();
+
+        if (spaceshipDetails.getName() != null)
+            spaceship.setName(spaceshipDetails.getName());
+
+        if (spaceshipDetails.getDescription() != null)
+            spaceship.setDescription(spaceshipDetails.getDescription());
+
+        if (spaceshipDetails.getOrigin() != null)
+            spaceship.setOrigin(spaceshipDetails.getOrigin());
+
+        Spaceship updatedSpaceship = spaceshipRepository.save(spaceship);
+
+        spaceShipProducer.sendSpaceShipMessage("nave " + updatedSpaceship.getSpaceshipId() + " actualizada");
+
+        return modelMapper.map(updatedSpaceship, SpaceshipDTO.class);
     }
 
 
+    @CacheEvict(value = "spaceships", key = "#id")
     public void deleteSpaceship(Long id) {
         spaceshipRepository.deleteById(id);
+        spaceShipProducer.sendSpaceShipMessage( "nave "+id+" eliminada" );
     }
 
 }
